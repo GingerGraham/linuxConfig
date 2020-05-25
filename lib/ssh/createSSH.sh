3cfa270b8625dbfb56bc441af3cf25aec1cfb5a3
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2020-05-25 13:25:56 +0100

    Added code for distro version and passphrase

--- lib/ssh/createSSH.sh
+++ lib/ssh/createSSH.sh
@@ -30,12 +30,64 @@ if [[ -z $host ]]; then
 	host=$(cat /etc/hostname)
 fi
 if [[ -z $distro ]]; then
-	# TODO complete distro population
+	if [ -f /etc/os-release ]; then
+		    # freedesktop.org and systemd
+			    . /etc/os-release
+				    distro=$NAME
+				elif type lsb_release >/dev/null 2>&1; then
+					    # linuxbase.org
+						    distro=$(lsb_release -si)
+						elif [ -f /etc/lsb-release ]; then
+							    # For some versions of Debian/Ubuntu without lsb_release command
+								    . /etc/lsb-release
+									    distro=$DISTRIB_ID
+									elif [ -f /etc/debian_version ]; then
+										    # Older Debian/Ubuntu/etc.
+											    distro=Debian
+											elif [ -f /etc/SuSe-release ]; then
+												    # Older SuSE/etc.
+													    # TODO replace with appropriate code
+													elif [ -f /etc/redhat-release ]; then
+														    # Older Red Hat, CentOS, etc.
+															    # TODO replace with appropriate code
+															else
+																    # Fall back to uname, e.g.
+																	# "Linux <version>", also works
+																	# for BSD, etc.
+																	    distro=$(uname -s)
+	fi
 fi
 if [[ -z $version ]]; then
-	# TODO complete version population
+	if [ -f /etc/os-release ]; then
+		    # freedesktop.org and systemd
+			    . /etc/os-release
+				    version=$VERSION_ID
+				elif type lsb_release >/dev/null 2>&1; then
+					    # linuxbase.org
+						    version=$(lsb_release -sr)
+						elif [ -f /etc/lsb-release ]; then
+							    # For some versions of Debian/Ubuntu without lsb_release command
+								    . /etc/lsb-release
+									    version=$DISTRIB_RELEASE
+									elif [ -f /etc/debian_version ]; then
+										    # Older Debian/Ubuntu/etc.
+											    version=$(cat /etc/debian_version)
+											elif [ -f /etc/SuSe-release ]; then
+												    # Older SuSE/etc.
+													    # TODO replace with appropriate code
+													elif [ -f /etc/redhat-release ]; then
+														    # Older Red Hat, CentOS, etc.
+															    # TODO replace with appropriate code
+															else
+																    # Fall back to uname, e.g.
+																	# "Linux <version>", also works
+																	# for BSD, etc.
+																	    version=$(uname -r)
+	fi
 fi
 if [[ -z $passphrase ]]; then
-	# TODO complete passphrase population
 	# Verify if they intended to supply a blank passphrase and allow if yes
+	echo -n "No passphrase given! Enter a passphrase now, or press ENTER to continue with no
+	passphrase: "
+	read -s passphrase
 fi
